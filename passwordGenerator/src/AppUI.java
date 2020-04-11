33888939b1eff73c51934e2457aff7e20f9dc86d
Denis Kyptiyanov <iaaarn@yahoo.com>
2020-04-11 21:35:23 +0300

    V1.0.0.2

---
+++ passwordGenerator/src/AppUI.java
@@ -0,0 +1,86 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AppUI extends JFrame {
+    JButton button = new JButton("Generate");
+
+    JTextField text= new JTextField( 12);
+    JTextField area = new JTextField( 12);
+    JCheckBox checkBox1 = new JCheckBox("Upper Case", false);
+    JCheckBox checkBox2 = new JCheckBox("Digits", false);
+    JCheckBox checkBox3 = new JCheckBox("Special chars", false);
+
+
+    private AppUI()
+    {
+        super("Password Generator");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        text.setToolTipText("Set password length");
+        text.addActionListener(new ListenerInput());
+        button.addActionListener(new ListenerAction());
+        checkBox1.addActionListener(new ListenerCheckBox1());
+        checkBox2.addActionListener(new ListenerCheckBox2());
+        checkBox3.addActionListener(new ListenerCheckBox3());
+        area.setText("Your Password : ");
+
+        JPanel conteiner = new JPanel();
+        conteiner.add(text);
+        conteiner.add(area);
+        conteiner.add(button);
+        conteiner.add(checkBox1);
+        conteiner.add(checkBox2);
+        conteiner.add(checkBox3);
+
+        setContentPane(conteiner);
+        setSize(200, 200);
+        setVisible(true);
+    }
+    private class ListenerCheckBox1 implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent a) {
+            if(checkBox1.isSelected()){
+                algorithm2.setBox1(1);
+            }
+        }
+    }private class ListenerCheckBox2 implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent a) {
+            if(checkBox2.isSelected()){
+                algorithm2.setBox2(1);
+            }
+
+        }
+    }private class ListenerCheckBox3 implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent a) {
+            if(checkBox3.isSelected()){
+                algorithm2.setBox3(1);
+            }
+        }
+    }
+    private class ListenerInput implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent a) {
+            String temp = text.getText();
+            int num = Integer.parseInt(temp);
+        }
+    }
+    private class ListenerAction implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent a) {
+            area.setText(algorithm2.passGen(Integer.parseInt(text.getText())));
+            algorithm2.clearArray(true);
+        }
+    }
+
+
+
+    public static void main (String[]args){
+        AppUI app = new AppUI();
+
+
+    }
+
+}
+
