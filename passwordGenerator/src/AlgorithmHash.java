9a7c4e240054296b85be1f23b771c4f44b75891f
Denis Kyptiyanov <iaaarn@yahoo.com>
2020-04-24 17:30:50 +0300

    V1.0.2.1  Clear code.

--- passwordGenerator/src/AlgorithmHash.java
+++ passwordGenerator/src/AlgorithmHash.java
@@ -8,7 +8,6 @@ public class AlgorithmHash {
     final static String upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
     final static String digits = "0123456789";
     final static String specSymbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}";
-    private static String text = "";
 
     private static ArrayList<String> putList(String text){
         ArrayList<String> list = new ArrayList<>();
@@ -19,39 +18,44 @@ public class AlgorithmHash {
         }
         return list;
     }
-public static Boolean CharsValidator (String text) {
+    public static Boolean CharsValidator (String text) {
+
         boolean temp = false;
-    if (text.isEmpty()) {
+
+        if (text.isEmpty()) {
         JOptionPane.showMessageDialog(null, "Error! Text box is empty! Please, try to enter a number");
     }
-    if (!isInteger(text)) {
+        if (!isInteger(text)) {
         JOptionPane.showMessageDialog(null, "Error! You entered the letter!  Please, try to enter a number");
     }
-    if ((Integer.parseInt(text) > 25) || Integer.parseInt(text) <= 0) {
+        if ((Integer.parseInt(text) > 25) || Integer.parseInt(text) <= 0) {
         JOptionPane.showMessageDialog(null, "Error! Entered number is out of range! Max length of password if 25 numbers.");
     }
-     else if (isInteger(text)) temp =true;
-    return temp; }
+        else if (isInteger(text)) temp =true;
+        return temp;
+    }
 
     public static String Algorithm (int number, boolean checkBox1, boolean checkBox2, boolean checkBox3){
+
         ArrayList<String> list = putList(standard);
+
         if(checkBox1)list.addAll(putList(upper));
         if(checkBox2)list.addAll(putList(digits));
         if(checkBox3)list.addAll(putList(specSymbols));
         String text = "";
-        Collections.shuffle(list);
-        for (int i=0; i<number;i++){
-            text=text.concat(list.get(i));}
-            return text;}
 
-    public static String getText() {
+        for (int i=0; i<number;i++){
+            text=text.concat(list.get((int)(Math.random()*list.size())));
+        }
         return text;
     }
 
     private static boolean isInteger(String s){
+
         try{
             Integer.parseInt(s);
-        }catch (NumberFormatException | NullPointerException n){
+        }
+        catch (NumberFormatException | NullPointerException n){
             return false;
         }
         return true;
--- passwordGenerator/src/AppUI.java
+++ passwordGenerator/src/AppUI.java
@@ -11,9 +11,7 @@ import java.awt.event.KeyAdapter;
 public class AppUI extends JFrame {
 
     JButton button = new JButton("Generate");
-
     JButton copy = new JButton("Copy");
-
     JTextField text= new JTextField( 6);
     JTextField area = new JTextField( 12);
     JCheckBox checkBox1 = new JCheckBox("Upper Case", false);
@@ -27,20 +25,24 @@ public class AppUI extends JFrame {
         text.setToolTipText("Set password length");
         button.addActionListener(new ListenerAction());
         copy.addActionListener(actionEvent -> {
+
             String ctc = area.getText();
             StringSelection stringSelection = new StringSelection(ctc);
             Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
             clpbrd.setContents(stringSelection, null);
         });
+
         text.addKeyListener(new KeyAdapter() {
             public void keyReleased(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                     if (AlgorithmHash.CharsValidator(text.getText()) == true) {
-                        area.setText(AlgorithmHash.Algorithm(Integer.parseInt(text.getText()), checkBox1.isSelected(), checkBox2.isSelected(), checkBox3.isSelected()));
+                        area.setText(AlgorithmHash.Algorithm(Integer.parseInt(text.getText()),
+                                checkBox1.isSelected(), checkBox2.isSelected(), checkBox3.isSelected()));
                     }
                 }
             }
         });
+
         checkBox1.addActionListener(new ListenerCheckBox1());
         checkBox2.addActionListener(new ListenerCheckBox2());
         checkBox3.addActionListener(new ListenerCheckBox3());
@@ -60,32 +62,32 @@ public class AppUI extends JFrame {
         setSize(200, 200);
         setVisible(true);
         setLocationRelativeTo(null);
+
     }
     private class ListenerCheckBox1 implements ActionListener{
 
         @Override
-        public void actionPerformed(ActionEvent a) {
-
-        }
+        public void actionPerformed(ActionEvent a) {        }
     }
+
     private class ListenerCheckBox2 implements ActionListener{
         @Override
-        public void actionPerformed(ActionEvent a) {
-
-        }
+        public void actionPerformed(ActionEvent a) {        }
     }
+
     private class ListenerCheckBox3 implements ActionListener{
         @Override
-        public void actionPerformed(ActionEvent a) {
-
-        }
+        public void actionPerformed(ActionEvent a) {        }
     }
+
     private class ListenerAction implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent a) {
             if (AlgorithmHash.CharsValidator(text.getText()) == true) {
-                area.setText(AlgorithmHash.Algorithm(Integer.parseInt(text.getText()), checkBox1.isSelected(), checkBox2.isSelected(), checkBox3.isSelected()));
-            }    }
+                area.setText(AlgorithmHash.Algorithm(Integer.parseInt(text.getText()),
+                        checkBox1.isSelected(), checkBox2.isSelected(), checkBox3.isSelected()));
+            }
+        }
     }
     public static void main (String[]args){
         new AppUI();
